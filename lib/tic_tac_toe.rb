940771d7c7e24f9a0ab41894ca086c52beea5b90
Zackary Cammack <zack.cammack@gmail.com>
2020-08-28 16:42:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,7 +50,14 @@ class TicTacToe
   end
 
   def turn
-    input = gets.chomp
-
-  end
-end
+   puts "Player #{current_player}, please enter a number 1-9:"
+   input = gets.strip
+   index = input_to_index(input)
+   cp = current_player
+   if valid_move?(index)
+     move(index, cp)
+     display_board
+   else
+     turn
+   end
+ end
